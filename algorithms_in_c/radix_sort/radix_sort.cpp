feb65056f00c95bb95cd279274469b729c8121ff
Erik Blenert <erik.blenert@gmail.com>
2023-03-13 01:17:42 +0000

    Fix radix sort.

---
+++ algorithms_in_c/radix_sort/radix_sort.cpp
@@ -0,0 +1,90 @@
+/**
+ * Source: https://www.geeksforgeeks.org/radix-sort/ 
+ */
+
+
+// C++ implementation of Radix Sort
+
+#include <iostream>
+using namespace std;
+
+extern "C" {
+    #include "../utils/read_from_file.h"
+}
+
+// A utility function to get maximum value in arr[]
+int getMax(int arr[], int n)
+{
+	int mx = arr[0];
+	for (int i = 1; i < n; i++)
+		if (arr[i] > mx)
+			mx = arr[i];
+	return mx;
+}
+
+// A function to do counting sort of arr[] according to
+// the digit represented by exp.
+void countSort(int arr[], int n, int exp)
+{
+	int output[n]; // output array
+	int i, count[10] = { 0 };
+
+	// Store count of occurrences in count[]
+	for (i = 0; i < n; i++)
+		count[(arr[i] / exp) % 10]++;
+
+	// Change count[i] so that count[i] now contains actual
+	// position of this digit in output[]
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+	// Build the output array
+	for (i = n - 1; i >= 0; i--) {
+		output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+		count[(arr[i] / exp) % 10]--;
+	}
+
+	// Copy the output array to arr[], so that arr[] now
+	// contains sorted numbers according to current digit
+	for (i = 0; i < n; i++)
+		arr[i] = output[i];
+}
+
+// The main function to that sorts arr[] of size n using
+// Radix Sort
+void radixsort(int arr[], int n)
+{
+	// Find the maximum number to know number of digits
+	int m = getMax(arr, n);
+
+	// Do counting sort for every digit. Note that instead
+	// of passing digit number, exp is passed. exp is 10^i
+	// where i is current digit number
+	for (int exp = 1; m / exp > 0; exp *= 10)
+		countSort(arr, n, exp);
+}
+
+// A utility function to print an array
+void print(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+}
+
+// Driver Code
+int main(int argc, char *argv[])
+{
+    int *arr;
+    int n;
+    n = readFromFile(argv[1], &arr);
+
+    clock_t begin = clock();
+	radixsort(arr, n);
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Radix sort for %s took %fs\n", argv[1], time_spent);
+
+	free(arr);
+
+	return 0;
+}
